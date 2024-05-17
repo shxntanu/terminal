cc1c2729a7c67210f586e0368b998f9b0a960cf6
Shantanu Wable <97496261+shxntanu@users.noreply.github.com>
2024-05-17 19:07:08 +0530

    update about

--- src/components/commands/About.tsx
+++ src/components/commands/About.tsx
@@ -8,15 +8,17 @@ const About: React.FC = () => {
   return (
     <AboutWrapper data-testid="about">
       <p>
-        Hi, my name is <HighlightSpan>Sat Naing</HighlightSpan>!
+        Hi, my name is <HighlightSpan>Shantanu Wable</HighlightSpan>!
       </p>
       <p>
-        I'm <HighlightAlt>a full-stack developer</HighlightAlt> based in Yangon,
-        Myanmar.
+        I'm <HighlightAlt>a full-stack developer</HighlightAlt> based in Pune, India.
       </p>
-      <p>
-        I am passionate about writing codes and <br />
-        developing web applications to solve real-life challenges.
+      <p>      
+          I am a full stack Web Developer, primarily using the MERN Stack. <br/>
+          I have a strong foundation in Frontend development in React using Redux, Zustand and ContextAPI. <br/>
+          I also have good experience in Backend development using ExpressJS, Flask & Django <br/>
+          and also have dabbled in server-side technologies like Celery, message queues like RabbitMQ, <br/>
+          and Large Language Models like Llama2, Mixtral and DistilBERT.
       </p>
     </AboutWrapper>
   );
